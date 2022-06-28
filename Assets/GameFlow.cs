35d166c9bdd2e4b68cb689e532fa50c69200ed47
Aishwarya Mohan <aishmohan@gmail.com>
2022-06-27 23:45:04 -0500

    Spawned multiple customers by adding a variable and method at start

--- Assets/GameFlow.cs
+++ Assets/GameFlow.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class GameFlow : MonoBehaviour
 {
+    // Global variables
+    
+    public Transform customerObj;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawnCustomers();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    private void spawnCustomers()
+    {
+        for (int numPatron = 0; numPatron < 5; numPatron++)
+        {
+            Instantiate(customerObj, new Vector3(customerObj.position.x + (2.0f * numPatron), customerObj.position.y, customerObj.position.z), customerObj.rotation);
+        }
         
     }
 }
